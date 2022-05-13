1d4d5cbcdbd9afd35635fb97b1216f3123efe047
PamLeles <pamela_leles_sousa@outlook.com>
2022-05-13 17:25:40 -0300

    Adding component files and structure

--- src/Views/App.jsx
+++ src/Views/App.jsx
@@ -1,9 +1,10 @@
 import './App.css'
+import Menu from '../Components/Layout/Menu'
 import React from "react";
 
 const App = (props) => (
     <div className="App">
-        Compenente App
+        <Menu />
     </div>
 )
 export default App;
