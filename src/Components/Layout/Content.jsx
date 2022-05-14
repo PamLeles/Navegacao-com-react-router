66ca5163602fbd6934db0682ace4ba07f33332bb
PamLeles <pamela_leles_sousa@outlook.com>
2022-05-13 22:13:49 -0300

    Error 404, page not found

--- src/Components/Layout/Content.jsx
+++ src/Components/Layout/Content.jsx
@@ -5,15 +5,16 @@ import { Routes, Route } from "react-router-dom";
 import Home from "../../Views/Exemples/Home.jsx";
 import About from "../../Views/Exemples/About.jsx";
 import Param from "../../Views/Exemples/Param.jsx";
+import NotFound from "../../Views/Exemples/NotFound.jsx";
 
 const Content = (props) => {
     return (
         <main className="Content">
             <Routes>
                 <Route path="/about" element={<About />} />
-                <Route path="/param/:id" element={<Param />}/> 
+                <Route path="/param/:id" element={<Param />} />
                 <Route path="/" element={<Home />} />
-                {/* <Route path="*" element={<NotFound />} />*/}
+                <Route path="*" element={<NotFound />} />
             </Routes>
         </main>
     )
---
+++ src/Views/Exemples/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NotFound = (props) => {
+    return(
+        <div className="NotFound">
+        <h1>404</h1>
+        <h2> Opssss....  Páginas não encontrada!</h2>
+    </div>
+    )
+}
+export default NotFound;
